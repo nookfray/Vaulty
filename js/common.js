f8446d24e8c0787c48666e2293eb5ca4657d8b57
nookfray <nookfray@gmail.com>
2020-09-22 16:17:11 +0300

    правки 22.09.2020

--- js/common.js
+++ js/common.js
@@ -464,6 +464,7 @@ $(document).ready(function() {
 				},
 				email: {
 					email: true,
+					emailfull: true,
 					maxlength: 120,
 				},
 				emailre: {
@@ -577,6 +578,7 @@ $(document).ready(function() {
 				},
 				email: {
 					email: true,
+					emailfull: true,
 					maxlength: 120,
 				},
 				emailre: {
@@ -671,6 +673,7 @@ $(document).ready(function() {
 			rules:{
 				subscribe: {
 					email: true,
+					emailfull: true,
 					maxlength: 120,
 				},
 			},
@@ -695,6 +698,11 @@ $(document).ready(function() {
 			}
 		});
 	});
+
+
+	jQuery.validator.addMethod("emailfull", function(value, element) {
+		return this.optional(element) || /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i.test(value);
+	}, "Please enter valid email address!");
 
 
 
